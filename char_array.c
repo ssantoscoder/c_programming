53d49f5ca147f816712f3da9d03245f33fbb9cc1
ssantos <ssantoscoder0@gmail.com>
2024-10-22 21:39:23 -0600

    Initial commit

---
+++ char_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  char st[20];
+  st[0]  = 'H';
+  st[1]  = 'e';
+  st[2]  = 'l';
+  st[3]  = 'l';
+  st[4]  = 'o';
+  st[5]  = ',';
+  st[6]  = ' ';
+  st[7]  = 'w';
+  st[8]  = 'o';
+  st[9]  = 'r';
+  st[10]  = 'l';
+  st[11] = 'd';
+  st[12] = '!';
+  st[13] = '\n';
+  st[14] = 0;
+  printf("%s", st); 
+}
